9b4f9575c58392f956172fa058119e530d05683b
Raja Judeh <raja.judeh@zeiss.com>
2025-10-23 14:01:19 +0200

    Add db.js for creating a PostgreSQL connection pool

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  max: 10,
+  idleTimeoutMillis: 10000
+});
